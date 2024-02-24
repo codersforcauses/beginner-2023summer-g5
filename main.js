18bcde18853dd225ebc14e091bf753fe0907a7b7
Lili Liu <78537049+LiliLiu09@users.noreply.github.com>
2024-02-24 11:49:02 +0800

    fixed JS

--- main.js
+++ main.js
@@ -1,174 +1,144 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const topLayer = generateRandomCards();
-  const bottomLayer = generateRandomCards();
-  const slots = document.getElementById("slots");
-    for (let i = 0; i < 7; i++) {
-        let slot = document.createElement('div');
-        slot.className = 'slot';
-        slots.appendChild(slot);
+class Game {
+  constructor(originSource, bindElement) {
+      this.doc = document;
+        this.originSource = this.randomList([
+            ...Array(6).fill('images/pink.png'),
+            ...Array(6).fill('images/yellow.png'),
+            ...Array(6).fill('images/blue.png'),
+          ]);
+   
+      this.bindElement = bindElement || this.doc.body;
+      this.slotElements = []; 
+      this.slot = []; 
+      this.box = null;
+      this.init().then(this.startHandler.bind(this));
+  }
+  startHandler() {
+      this.box = this.$('#ew-box');
+      this.resetHandler();
+      // make two layers of cards, each layre has 9 cards
+      for (let layer = 0; layer < 2; layer++) {
+          for (let row = 0; row < 3; row++) {
+              for (let col = 0; col < 3; col++) {
+                  const item = this.create('div');
+                  item.className = `ew-box-item`;
+                   item.style.position = 'absolute';
+                   item.style.left = `${col * 6.5}rem`;
+                   item.style.top = `${row * 4}rem`;
+                  item.style.zIndex = layer; // divide the cards into two layers
+                  const imageIndex = row * 3 + col + layer * 9;
+                // set the background image
+                item.style.backgroundImage = `url(${this.originSource[imageIndex]})`; 
+                item.style.backgroundSize = 'cover'; 
+                item.setAttribute('data-image', this.originSource[imageIndex]);
+                item.addEventListener('click', () => this.cardClickHandler(item));
+                this.box.append(item);
+              }
+          }
+      }
+
+      const slotsContainer = this.create('div');
+      slotsContainer.className = 'slots-container'; // create a container for the slots
+          // create 5 slots
+          for (let i = 0; i < 5; i++) {
+            let slot = this.create('div');
+            slot.className = 'slot';
+            this.slotElements.push(slot); // add the slot to the slot elements array
+            slotsContainer.appendChild(slot);
+        }
+      // add the slots container to the box
+      this.box.appendChild(slotsContainer);
+  }
+
+
+
+  cardClickHandler(card) {
+    const image = card.getAttribute('data-image');
+    if (this.slot.length < 5) {
+        card.style.display = 'none';
+        const slotElement = this.slotElements[this.slot.length];
+        slotElement.style.backgroundImage = `url("${image}")`;
+        this.slot.push(image);
+
+        // check if there is a match
+        if (this.slot.length >= 3) {
+            const imagesCount = {};
+            this.slot.forEach(image => {
+                imagesCount[image] = (imagesCount[image] || 0) + 1;
+            });
+
+            const matchedImage = Object.keys(imagesCount).find(image => imagesCount[image] >= 3);
+            if (matchedImage) {
+                // remove matched color from the slot
+                this.slot = this.slot.filter(image => image !== matchedImage);
+                this.slotElements.forEach((slotElement, Index) => {
+                    if (slotElement.style.backgroundImage === matchedImage) {
+                        slotElement.style.backgroundImage = '';
+                        slotElement.removeAttribute('data-image');
+                    }
+                });
+                // update the slots after match
+                this.updateSlotsAfterMatch();
+            }
+        }
     }
-
-  displayLayer(topLayer, 'topLayer');
-  displayLayer(bottomLayer, 'bottomLayer');
-      // 为 topLayer 上的卡片注册点击事件
-  registerCardClickEvent(document.getElementById('topLayer'));
-});
-// 将注册事件的逻辑封装为一个函数
-function registerCardClickEvent(layerElement) {
-  Array.from(layerElement.getElementsByClassName('card')).forEach(card => {
-      console.log("Adding click event listener to card"); // 调试信息
-      card.addEventListener('click', function(event) {
-          moveToSlot(event.target);
-      });
-  });
 }
 
-
-  document.getElementById('topLayer').addEventListener('click', (event) => {
-      if (event.target.classList.contains('card')) {
-          const index = Array.from(document.getElementById('topLayer').children).indexOf(event.target);
-          moveCardToSlot(index, topLayer, bottomLayer, slots);
-          revealBottomCard(index);
-          checkForMatches(slots);
-      }
-  });
-
-
-    let cards = document.getElementsByClassName('card');
-    Array.from(cards).forEach(card => {
-        card.addEventListener('click', function (event){
-          moveCardToSlot(event.target);
+updateSlotsAfterMatch() {
+    this.slotElements.forEach(slotElement => {
+        slotElement.style.backgroundImage = ''; // remove the background image
+        slotElement.removeAttribute('data-image'); // remove the data-image attribute
     });
+    // refill the slots with the remaining images
+    this.slot.forEach((image, index) => {
+        const slotElement = this.slotElements[index];
+        slotElement.style.backgroundImage = `url("${image}")`; 
+        slotElement.setAttribute('data-image', image); 
     });
+}
 
-    function moveToSlot(card) {
-      // 找到第一个空的插槽
-      console.log("Moving card to slot");
-      let slots = document.getElementsByClassName('slot');
-      let targetSlot = null;
-      for (let slot of slots) {
-          if (!slot.hasChildNodes()) {
-              targetSlot = slot;
-              break;
-          }
+
+
+
+
+
+
+
+
+  resetHandler() {
+      this.box.innerHTML = '';
+      this.slot = [];
+  }
+  init() {
+      return new Promise(resolve => {
+          const template = `<div class="ew-box" id="ew-box" ></div>`;
+          this.bindElement.innerHTML += template;
+          resolve();
+      })
+  }
+  randomList(arr) {
+      const newArr = [...arr];
+      for (let i = newArr.length - 1; i >= 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
       }
-  
-      if (targetSlot) {
-          // 创建移动动画
-          animateCardMove(card, targetSlot, () => {
-              // 动画结束后的操作
-              targetSlot.appendChild(card);
-              // 可以在这里添加检查匹配和更新游戏状态的代码
-          });
-      }
+      return newArr;
   }
-  
-  function animateCardMove(card, targetSlot, callback) {
-      console.log("Animating card move");
-      // 获取起始和结束的位置
-      let cardRect = card.getBoundingClientRect();
-      let slotRect = targetSlot.getBoundingClientRect();
-  
-      // 计算移动的距离
-      let translateX = slotRect.left - cardRect.left;
-      let translateY = slotRect.top - cardRect.top;
-  
-      // 应用CSS动画
-      card.style.transition = 'transform 0.5s ease';
-      card.style.transform = `translate(${translateX}px, ${translateY}px)`;
-  
-      // 动画结束后的清理
-      card.addEventListener('transitionend', function() {
-          card.style.transition = '';
-          card.style.transform = '';
-          callback();
-      }, { once: true });
+  $(selector) {
+      return this.doc.querySelector(selector);
   }
-function generateRandomCards() {
-  const colors = ['red', 'yellow', 'blue'];
-  let cards = [];
-  for (let color of colors) {
-      for (let i = 0; i < 6; i++) {
-          cards.push(color);
-      }
+  resetGame() {
+      // restart the game
+      this.resetHandler();
+      this.startHandler();
   }
-  shuffleArray(cards);
-  return cards.slice(0, 9);
-}
-
-function displayLayer(cards, layerId) {
-  const layerElement = document.getElementById(layerId);
-  cards.forEach(color => {
-      let cardElement = document.createElement('div');
-      cardElement.classList.add('card');
-      cardElement.style.backgroundColor = color;
-      layerElement.appendChild(cardElement);
-
-      // 添加点击事件监听器
-      cardElement.addEventListener('click', function(event) {
-          moveToSlot(event.target);
-      });
-  });
-}
-
-
-function moveCardToSlot(index, topLayer, bottomLayer, slots) {
-  const selectedCard = topLayer[index];
-  if (selectedCard) {
-      for (let i = 0; i < slots.length; i++) {
-          if (slots[i] === null) {
-              slots[i] = selectedCard;
-              topLayer[index] = null;
-              updateSlotDisplay(i, selectedCard);
-              break;
-          }
-      }
+  create(name) {
+      return this.doc.createElement(name);
   }
 }
-
-function revealBottomCard(index) {
-  document.getElementById('bottomLayer').children[index].style.visibility = 'visible';
+window.onload = () => {
+  const images = ['images/pink.png', 'images/pink.png', 'images/pink.png', 'images/pink.png', 'images/pink.png', 'images/pink.png', 'images/yellow.png', 'images/yellow.png', 'images/yellow.png', 'images/yellow.png', 'images/yellow.png', 'images/yellow.png', 'images/blue.png', 'images/blue.png', 'images/blue.png', 'images/blue.png', 'images/blue.png', 'images/blue.png'];
+  const game = new Game(images);
 }
 
-function checkForMatches(slots) {
-  let colorCount = {'red': 0, 'yellow': 0, 'blue': 0};
-  for (let color of slots) {
-      if (color) {
-          colorCount[color]++;
-      }
-  }
-
-  for (let color in colorCount) {
-      if (colorCount[color] >= 3) {
-          removeThreeCards(color, slots);
-          break;
-      }
-  }
-}
-
-function removeThreeCards(cardColor, slots) {
-  let count = 0;
-  for (let i = 0; i < slots.length; i++) {
-      if (slots[i] === cardColor) {
-          slots[i] = null;
-          updateSlotDisplay(i, null);
-          count++;
-          if (count === 3) {
-              break;
-          }
-      }
-  }
-}
-
-function updateSlotDisplay(slotIndex, color) {
-  const slot = document.getElementById('slots').children[slotIndex];
-  slot.style.backgroundColor = color ? color : '#fff';
-}
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-  }
-}
 
